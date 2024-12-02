d690edf636ac8e7d1a238f151816382f31a924a1
Rexomnes76 <belac76@gmail.com>
2024-12-02 00:27:40 -0500

    Added Nuzlocke and Randomizer Plugins

---
+++ Plugins/Random X/Config.rb
@@ -0,0 +1,16 @@
+#===============================================================================
+#  Randomizer Functionality for vanilla Essentials
+#  By Luka S.J Updated by A.I.R for v21.1
+#-------------------------------------------------------------------------------
+#  System settings
+#===============================================================================
+module Randomizer
+  # list of trainers to exclude from the randomizer
+  EXCLUSIONS_TRAINERS = []
+
+  # list of species to exclude from the randomizer
+  EXCLUSIONS_SPECIES = []
+
+  # list of items to exclude from the randomizer
+  EXCLUSIONS_ITEMS = [:SHEARS, :PICKAXE, :POWERGLOVES, :SURFBOARD, :CLIMBINGGEAR, :HM06]
+end
